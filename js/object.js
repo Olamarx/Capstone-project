ac0944f86fd6f14cc4612db3103e7e0405e6f2f9
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-04-12 03:33:59 +0100

    Add the mobile version of the curriculum

--- js/object.js
+++ js/object.js
@@ -1,50 +1,33 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 const mainCurriculum = [
     {
       id: 'card1',
       font: '<i class="fa fas-brands fa-html5"></i>',
       name: 'HTML',
-      description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+      description: " Hypertext Markup Language is the starting point",
     },
     {
       id: 'card2',
-      font: '<i class="fa fas-brands fa-css3-alt"></i>',
+      font: '<i class="fab fa-css3-alt fa-1x"></i>',
       name: 'CSS',
-      description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+      description: "CSS is known as Cascading Style Sheet and it is responsible for the syling of the site.",
     },
     {
       id: 'card3',
-      font: '<i class="fa fas-brands fa-js-square"></i>',
+      font: '<i class="fab fa-js fa-1x"></i>',
       name: 'Javascript',
-      description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+      description: "Javascript is responsible for the functionality and interactiveness of the website.",
     },
     {
       id: 'card4',
-      font: '<i class="fa fas-brands fa-git-alt"></i>',
+      font: '<i class="fab fa-git-alt fa-1x"></i>',
       name: 'Git/Github',
-      description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+      description: "Learn the usage of Git, a version control system.",
     },
     {
       id: 'card5',
-      font: '<i class="fa fas-solid fa-square-terminal"></i>',
+      font: '<i class="fas fa-terminal fa-1x"></i>',
       name: 'CLI',
-      description: "A daily selection of privately personalized reads; no accounts or sign-ups required. has been the industry's standard",
+      description: "CLI is the command line interface. Learn how to use the interface proficiently.",
     },
   ];
   
@@ -53,8 +36,58 @@ const mainCurriculum = [
 
   const afterHeader = document.querySelector('.bigger-body');
 
-afterHeader.insertAdjacentHTML('afterend', `
+    afterHeader.insertAdjacentHTML('afterend', `
+
+    <main class="main-curriculum">
+    <div class="main-curriculum-bg">
+        <div class="body-content">
+        <div class="body-content-block">
+            <h3 class="main-curriculum-h3"> Main Curriculum </h3>
+            <hr class="hr">
+        </div>
+    </div>
+<div class="center-lang-body">
+          <div class="languages-body"> 
+              <div class="languages">
+              <div class="font-for-lang">${mainCurriculum[0].font}</div>
+                <h3 class="language-h3">${mainCurriculum[0].name}</h3>
+                <p class="languages-para">${mainCurriculum[0].description}</p>
+            </div>
+
+            <div class="languages">
+            <div class="font-for-lang">${mainCurriculum[1].font}</div>
+            <h3 class="language-h3">${mainCurriculum[1].name}</h3>
+                <p class="languages-para">${mainCurriculum[1].description}</p>
+            </div>
+
+            <div class="languages">
+            <div class="font-for-lang">${mainCurriculum[2].font}</div>
+                <h3 class="language-h3">${mainCurriculum[2].name}</h3>
+                <p class="languages-para">${mainCurriculum[2].description}</p>
+            </div>
 
 
 
-`)
+            <div class="languages">
+                <div class="font-for-lang">${mainCurriculum[3].font}</div>
+                <h3 class="language-h3">${mainCurriculum[3].name}</h3>
+                <p class="languages-para">${mainCurriculum[3].description}</p>
+            </div>
+
+            <div class="languages">
+            <div class="font-for-lang">${mainCurriculum[4].font}</div>
+                <h3 class="language-h3">${mainCurriculum[4].name}</h3>
+                <p class="languages-para">${mainCurriculum[4].description}</p>
+            </div>
+
+</div>
+            
+    </div>
+<div class="whole-program-container">
+            <a href="#" class="whole-program"> SEE THE WHOLE PROGRAMME</a>
+           </div>
+        
+    </div>
+</main>
+
+`);
