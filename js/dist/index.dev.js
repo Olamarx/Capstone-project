346b32848840cca7ac6fb261f6f78c5000fbf752
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-04-13 22:03:29 +0100

    Fix the linter errors

--- js/dist/index.dev.js
+++ js/dist/index.dev.js
@@ -3,8 +3,6 @@
 var openNav = document.querySelector('.open-menu-nav');
 var closeNav = document.querySelector('.close-menu-nav');
 var asideBar = document.querySelector('.aside-content');
-var hr = document.querySelector('.hr');
-var closeTheDiv = document.querySelector('.open-nav');
 openNav.addEventListener('click', function () {
   asideBar.classList.toggle('active');
   openNav.style.display = 'none';
--- js/index.js
+++ js/index.js
@@ -1,8 +1,6 @@
 const openNav = document.querySelector('.open-menu-nav');
 const closeNav = document.querySelector('.close-menu-nav');
 const asideBar = document.querySelector('.aside-content');
-const hr = document.querySelector('.hr');
-const closeTheDiv = document.querySelector('.open-nav')
 
 openNav.addEventListener('click', () => {
   asideBar.classList.toggle('active');
