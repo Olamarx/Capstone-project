81059765758073c75d52f6eb2887f02805fc4118
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-04-14 21:20:30 +0100

    Add the card dynamically

--- js/dist/featured_teachers.dev.js
+++ js/dist/featured_teachers.dev.js
@@ -1,53 +1,50 @@
 "use strict";
 
-var teachers = [{
-  id: 'card1',
+var teacher1 = {
   name: 'Colleen van Lent Ph.D',
   title: 'Teacher at Coursera',
-  image: '',
   description: 'She is a lecturer at School of Information, University of Michigan and she teaches basics of HTML, CSS and Javascript on Coursera',
-  seeLive: '#',
-  seeSource: '#'
-}, {
-  id: 'card2',
+  featuredImage: 'colleen-van.jpeg'
+};
+var teacher2 = {
   name: 'Yaakov Chaikin',
   title: 'Instructor at Coursera',
-  image: '',
   description: "Each of Yaakov's Coursera courses have made it to be a #1 TOP RATED COURSE on Coursera.org. He lectures at Johns Hopkins University and builds web applications for client/customers in person.",
-  seeLive: '#',
-  seeSource: '#'
-}, {
-  id: 'card3',
+  featuredImage: 'yaakov.jpg'
+};
+var teacher3 = {
   name: 'Nancy Hughes',
   title: 'Teaches Web Design in New',
-  image: '',
   description: 'She has to her credit many designs on figma. She has also taught many successful Web Designers',
-  seeLive: '#',
-  seeSource: '#'
-}, {
-  id: 'card4',
+  featuredImage: 'nancy.jpg'
+};
+var teacher4 = {
   name: 'Emily Brown',
   title: 'A Michigan CSS expert',
-  image: '',
   description: 'She takes our student from nothing to something in CSS. By nothing to nothing, she ensures that you get the skill to that of the international standard.',
-  seeLive: '#',
-  seeSource: '#'
-}, {
-  id: 'card5',
+  featuredImage: 'emili.jpg'
+};
+var teacher5 = {
   name: 'Stephen Wright',
   title: 'CLI expert',
-  image: '',
   description: 'He uses the command lines on a daily, he has all you need to become a successful Web developer from the angle of CLI.',
-  seeLive: '#',
-  seeSource: '#'
-}, {
-  id: 'card6',
+  featuredImage: 'steven.jpg'
+};
+var teacher6 = {
   name: 'Andrew Brown',
   title: 'Frontend Developer',
-  image: '',
   description: 'He was a Google enplyee as a junior developer, now he teaches our student to Google standard.',
-  seeLive: '#',
-  seeSource: '#'
-}];
-var featuredTeachers = document.querySelector('.main-curriculum');
-featuredTeachers.insertAdjacentHTML('afterend', "\n<section class=\"featured-teacher\"> \n    <div class=\"featured-teachers-center\">\n        <h3 class=\"featured-teachers-h3\">\n            Featured Teachers\n        </h3>\n        <hr class=\"hr\">\n    </div>\n<div class=\"body-of-whole-card\">\n\n\n\n    <div class=\"cards\">\n        <div class=\"card-image\"> <div class=\"card-image-2 card-1\">\n        </div>\n        </div>\n\n       <div class=\"image-content-body\">\n        <div class=\"image-content\">\n            <h3 class=\"name\">\n                ".concat(teachers[0].name, "\n            </h3>\n            \n            <div class=\"course-taken\">\n                <h5 class=\"course-taken-h5\">\n                ").concat(teachers[0].title, "\n                </h5>\n            </div>\n            <hr class=\"teacher-hr\">\n        </div>\n\n        <div class=\"teacher-content-para\">\n            <p class=\"teacher-content-paragraph\"> ").concat(teachers[0].description, " </p>\n        </div>\n       </div>\n    </div>\n\n    <div class=\"cards\">\n        <div class=\"card-image\"> <div class=\"card-image-2  card-2\">\n        </div>\n        </div>\n\n       <div class=\"image-content-body\">\n        <div class=\"image-content\">\n            <h3 class=\"name\">\n            ").concat(teachers[1].name, "\n            </h3>\n            \n            <div class=\"course-taken\">\n                <h5 class=\"course-taken-h5\">\n                ").concat(teachers[1].title, "\n                </h5>\n            </div>\n            <hr class=\"teacher-hr\">\n        </div>\n\n        <div class=\"teacher-content-para\">\n            <p class=\"teacher-content-paragraph\">\n            ").concat(teachers[1].description, "</p>\n        </div>\n       </div>\n    </div>\n\n    <div class=\"cards\">\n        <div class=\"card-image\"> <div class=\"card-image-2  card-5\">\n        </div>\n        </div>\n\n       <div class=\"image-content-body\">\n        <div class=\"image-content\">\n            <h3 class=\"name\">\n            ").concat(teachers[4].name, "\n            </h3>\n            \n            <div class=\"course-taken\">\n                <h5 class=\"course-taken-h5\">\n                ").concat(teachers[4].title, "\n                </h5>\n            </div>\n            <hr class=\"teacher-hr\">\n        </div>\n\n        <div class=\"teacher-content-para\">\n            <p class=\"teacher-content-paragraph\">\n            ").concat(teachers[4].description, "  </p>\n        </div>\n       </div>\n    </div>\n\n\n    <div class=\"cards\">\n        <div class=\"card-image\"> <div class=\"card-image-2 card-6\">\n        </div>\n        </div>\n\n       <div class=\"image-content-body\">\n        <div class=\"image-content\">\n            <h3 class=\"name\">\n            ").concat(teachers[5].name, "\n            </h3>\n            \n            <div class=\"course-taken\">\n                <h5 class=\"course-taken-h5\">\n                ").concat(teachers[5].title, "\n                </h5>\n            </div>\n            <hr class=\"teacher-hr\">\n        </div>\n\n        <div class=\"teacher-content-para\">\n            <p class=\"teacher-content-paragraph\">\n            ").concat(teachers[5].description, " </p>\n            </div>\n            </div>\n            </div>\n            <div class=\"cards\">\n                <div class=\"card-image\"> <div class=\"card-image-2  card-3\">\n                </div>\n                </div>\n            \n               <div class=\"image-content-body\">\n                <div class=\"image-content\">\n                    <h3 class=\"name\">\n                    ").concat(teachers[2].name, "\n                    </h3>\n                    \n                    <div class=\"course-taken\">\n                        <h5 class=\"course-taken-h5\">\n                        ").concat(teachers[2].title, "\n                        </h5>\n                    </div>\n                    <hr class=\"teacher-hr\">\n                </div>\n            \n                <div class=\"teacher-content-para\">\n                    <p class=\"teacher-content-paragraph\">\n                    ").concat(teachers[2].description, "         </p>\n                </div>\n                </div>\n                </div>\n                \n                \n                <div class=\"cards\">\n                    <div class=\"card-image\"> <div class=\"card-image-2  card-4\">\n                    </div>\n                    </div>\n            \n                   <div class=\"image-content-body\">\n                   <div class=\"image-content\">\n                   <h3 class=\"name\">\n                   ").concat(teachers[3].name, "\n                   </h3>\n                   \n                   <div class=\"course-taken\">\n                            <h5 class=\"course-taken-h5\">\n                            ").concat(teachers[3].title, "\n                            </h5>\n                        </div>\n                        <hr class=\"teacher-hr\">\n                    </div>\n            \n                    <div class=\"teacher-content-para\">\n                        <p class=\"teacher-content-paragraph\">\n                        ").concat(teachers[3].description, "    </p>\n                    </div>\n                   </div>\n                </div>\n                </div>\n                </section>\n\n            <!---- Partners ---->\n\n                <section class=\"partner-section\">\n    <div class=\"partner-content\">\n        <h3 class=\"partner-h3\">\n            Partner\n        </h3>\n        <hr class=\"partner-hr\">\n\n        <div class=\"adverts\">\n    <span class=\"font-and-content\">\n        <i class=\"fab fa-google\"> </i> Google\n    </span>\n    <span class=\"font-and-content\">\n        <i class=\"fab fa-airbnb\"> </i> Airbnb\n    </span>\n    <span class=\"font-and-content\">\n        <i class=\"fab fa-microsoft\"> </i> Microsoft\n    </span>\n    <span class=\"font-and-content\">\n        <i class=\"fab fa-apple\"> </i>Apple\n    </span>\n    <span class=\"font-and-content\">\n        <i class=\"fab fa-free-code-camp\"> </i>Apple\n    </span>\n        </div>\n    </div>\n</section>\n"));
+  featuredImage: 'andrew.jpg'
+};
+var teachers = [teacher1, teacher2, teacher3, teacher4, teacher5, teacher6];
+
+var generateCards = function generateCards() {
+  var placeInHTML = document.querySelector('.featured-teacher');
+  teachers.forEach(function (cd) {
+    placeInHTML.insertAdjacentHTML('beforeend', "\n        <li class=\"li-list\">\n        <div class=\"imageInd\">\n        <img class=\"images-cards\" src=\"./images/".concat(cd.featuredImage, "\" alt=\"").concat(cd.name, "\">\n        </div>\n        <div class=\"description\">\n          <h3>").concat(cd.name, "</h3>\n          <h4>").concat(cd.title, " </h4>\n          <hr>\n          <p>").concat(cd.description, "</p>\n        </div>\n    </li>\n    "));
+  });
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  generateCards();
+});
--- js/featured_teachers.js
+++ js/featured_teachers.js
@@ -1,258 +1,80 @@
-const teachers = [
-  {
-    id: 'card1',
+const teacher1 = {
     name: 'Colleen van Lent Ph.D',
     title: 'Teacher at Coursera',
-    image: '',
     description: 'She is a lecturer at School of Information, University of Michigan and she teaches basics of HTML, CSS and Javascript on Coursera',
-    seeLive: '#',
-    seeSource: '#',
-  },
-  {
-    id: 'card2',
-    name: 'Yaakov Chaikin',
-    title: 'Instructor at Coursera',
-    image: '',
-    description: "Each of Yaakov's Coursera courses have made it to be a #1 TOP RATED COURSE on Coursera.org. He lectures at Johns Hopkins University and builds web applications for client/customers in person.",
-    seeLive: '#',
-    seeSource: '#',
-  },
-  {
-    id: 'card3',
-    name: 'Nancy Hughes',
-    title: 'Teaches Web Design in New',
-    image: '',
-    description: 'She has to her credit many designs on figma. She has also taught many successful Web Designers',
-    seeLive: '#',
-    seeSource: '#',
-  },
-  {
-    id: 'card4',
+    featuredImage: 'colleen-van.jpeg',
+}
+
+const teacher2 = {
+        name: 'Yaakov Chaikin',
+        title: 'Instructor at Coursera',
+        description: "Each of Yaakov's Coursera courses have made it to be a #1 TOP RATED COURSE on Coursera.org. He lectures at Johns Hopkins University and builds web applications for client/customers in person.",
+    featuredImage: 'yaakov.jpg',
+    }
+
+const teacher3 = {
+        name: 'Nancy Hughes',
+        title: 'Teaches Web Design in New',
+        description: 'She has to her credit many designs on figma. She has also taught many successful Web Designers',
+        featuredImage: 'nancy.jpg',
+}
+
+const teacher4 = {
     name: 'Emily Brown',
     title: 'A Michigan CSS expert',
-    image: '',
-    description: 'She takes our student from nothing to something in CSS. By nothing to nothing, she ensures that you get the skill to that of the international standard.',
-    seeLive: '#',
-    seeSource: '#',
-  },
-  {
-    id: 'card5',
+    description: 'She takes our student from nothing to something in CSS. By nothing to nothing, she ensures that you get the skill to that of the international standard.',    
+    featuredImage: 'emili.jpg',
+}
+
+const teacher5 = {
     name: 'Stephen Wright',
     title: 'CLI expert',
-    image: '',
-    description: 'He uses the command lines on a daily, he has all you need to become a successful Web developer from the angle of CLI.',
-    seeLive: '#',
-    seeSource: '#',
-  },
-  {
-    id: 'card6',
+    description: 'He uses the command lines on a daily, he has all you need to become a successful Web developer from the angle of CLI.',    
+    featuredImage: 'steven.jpg',
+}
+
+
+const teacher6 = {
     name: 'Andrew Brown',
     title: 'Frontend Developer',
-    image: '',
     description: 'He was a Google enplyee as a junior developer, now he teaches our student to Google standard.',
-    seeLive: '#',
-    seeSource: '#',
-  },
-];
-
-const featuredTeachers = document.querySelector('.main-curriculum');
-
-featuredTeachers.insertAdjacentHTML('afterend', `
-<section class="featured-teacher"> 
-    <div class="featured-teachers-center">
-        <h3 class="featured-teachers-h3">
-            Featured Teachers
-        </h3>
-        <hr class="hr">
-    </div>
-<div class="body-of-whole-card">
+    featuredImage: 'andrew.jpg',
+}
 
 
 
-    <div class="cards">
-        <div class="card-image"> <div class="card-image-2 card-1">
+const teachers = [
+    teacher1,
+    teacher2,
+    teacher3,
+    teacher4,
+    teacher5,
+    teacher6,
+]
+
+
+const generateCards = () => {
+    const placeInHTML = document.querySelector('.featured-teacher')
+    teachers.forEach((cd) => {
+        placeInHTML.insertAdjacentHTML('beforeend',
+        `
+        <li class="li-list">
+        <div class="imageInd">
+        <img class="images-cards" src="./images/${cd.featuredImage}" alt="${cd.name}">
         </div>
+        <div class="description">
+          <h3>${cd.name}</h3>
+          <h4>${cd.title} </h4>
+          <hr>
+          <p>${cd.description}</p>
         </div>
+    </li>
+    `
+      )    
+    });
+};
 
-       <div class="image-content-body">
-        <div class="image-content">
-            <h3 class="name">
-                ${teachers[0].name}
-            </h3>
-            
-            <div class="course-taken">
-                <h5 class="course-taken-h5">
-                ${teachers[0].title}
-                </h5>
-            </div>
-            <hr class="teacher-hr">
-        </div>
+document.addEventListener("DOMContentLoaded", () => {
+    generateCards();
+});
 
-        <div class="teacher-content-para">
-            <p class="teacher-content-paragraph"> ${teachers[0].description} </p>
-        </div>
-       </div>
-    </div>
-
-    <div class="cards">
-        <div class="card-image"> <div class="card-image-2  card-2">
-        </div>
-        </div>
-
-       <div class="image-content-body">
-        <div class="image-content">
-            <h3 class="name">
-            ${teachers[1].name}
-            </h3>
-            
-            <div class="course-taken">
-                <h5 class="course-taken-h5">
-                ${teachers[1].title}
-                </h5>
-            </div>
-            <hr class="teacher-hr">
-        </div>
-
-        <div class="teacher-content-para">
-            <p class="teacher-content-paragraph">
-            ${teachers[1].description}</p>
-        </div>
-       </div>
-    </div>
-
-    <div class="cards">
-        <div class="card-image"> <div class="card-image-2  card-5">
-        </div>
-        </div>
-
-       <div class="image-content-body">
-        <div class="image-content">
-            <h3 class="name">
-            ${teachers[4].name}
-            </h3>
-            
-            <div class="course-taken">
-                <h5 class="course-taken-h5">
-                ${teachers[4].title}
-                </h5>
-            </div>
-            <hr class="teacher-hr">
-        </div>
-
-        <div class="teacher-content-para">
-            <p class="teacher-content-paragraph">
-            ${teachers[4].description}  </p>
-        </div>
-       </div>
-    </div>
-
-
-    <div class="cards">
-        <div class="card-image"> <div class="card-image-2 card-6">
-        </div>
-        </div>
-
-       <div class="image-content-body">
-        <div class="image-content">
-            <h3 class="name">
-            ${teachers[5].name}
-            </h3>
-            
-            <div class="course-taken">
-                <h5 class="course-taken-h5">
-                ${teachers[5].title}
-                </h5>
-            </div>
-            <hr class="teacher-hr">
-        </div>
-
-        <div class="teacher-content-para">
-            <p class="teacher-content-paragraph">
-            ${teachers[5].description} </p>
-            </div>
-            </div>
-            </div>
-            <div class="cards">
-                <div class="card-image"> <div class="card-image-2  card-3">
-                </div>
-                </div>
-            
-               <div class="image-content-body">
-                <div class="image-content">
-                    <h3 class="name">
-                    ${teachers[2].name}
-                    </h3>
-                    
-                    <div class="course-taken">
-                        <h5 class="course-taken-h5">
-                        ${teachers[2].title}
-                        </h5>
-                    </div>
-                    <hr class="teacher-hr">
-                </div>
-            
-                <div class="teacher-content-para">
-                    <p class="teacher-content-paragraph">
-                    ${teachers[2].description}         </p>
-                </div>
-                </div>
-                </div>
-                
-                
-                <div class="cards">
-                    <div class="card-image"> <div class="card-image-2  card-4">
-                    </div>
-                    </div>
-            
-                   <div class="image-content-body">
-                   <div class="image-content">
-                   <h3 class="name">
-                   ${teachers[3].name}
-                   </h3>
-                   
-                   <div class="course-taken">
-                            <h5 class="course-taken-h5">
-                            ${teachers[3].title}
-                            </h5>
-                        </div>
-                        <hr class="teacher-hr">
-                    </div>
-            
-                    <div class="teacher-content-para">
-                        <p class="teacher-content-paragraph">
-                        ${teachers[3].description}    </p>
-                    </div>
-                   </div>
-                </div>
-                </div>
-                </section>
-
-            <!---- Partners ---->
-
-                <section class="partner-section">
-    <div class="partner-content">
-        <h3 class="partner-h3">
-            Partner
-        </h3>
-        <hr class="partner-hr">
-
-        <div class="adverts">
-    <span class="font-and-content">
-        <i class="fab fa-google"> </i> Google
-    </span>
-    <span class="font-and-content">
-        <i class="fab fa-airbnb"> </i> Airbnb
-    </span>
-    <span class="font-and-content">
-        <i class="fab fa-microsoft"> </i> Microsoft
-    </span>
-    <span class="font-and-content">
-        <i class="fab fa-apple"> </i>Apple
-    </span>
-    <span class="font-and-content">
-        <i class="fab fa-free-code-camp"> </i>Apple
-    </span>
-        </div>
-    </div>
-</section>
-`);
