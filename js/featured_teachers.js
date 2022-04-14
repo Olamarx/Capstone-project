1ea5229074c0e341dee848db26ec15e488238af9
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-04-14 21:53:46 +0100

    Fix the linters

--- js/featured_teachers.js
+++ js/featured_teachers.js
@@ -1,63 +1,59 @@
 const teacher1 = {
-    name: 'Colleen van Lent Ph.D',
-    title: 'Teacher at Coursera',
-    description: 'She is a lecturer at School of Information, University of Michigan and she teaches basics of HTML, CSS and Javascript on Coursera',
-    featuredImage: 'colleen-van.jpeg',
-}
+  name: 'Colleen van Lent Ph.D',
+  title: 'Teacher at Coursera',
+  description: 'She is a lecturer at School of Information, University of Michigan and she teaches basics of HTML, CSS and Javascript on Coursera',
+  featuredImage: 'colleen-van.jpeg',
+};
 
 const teacher2 = {
-        name: 'Yaakov Chaikin',
-        title: 'Instructor at Coursera',
-        description: "Each of Yaakov's Coursera courses have made it to be a #1 TOP RATED COURSE on Coursera.org. He lectures at Johns Hopkins University and builds web applications for client/customers in person.",
-    featuredImage: 'yaakov.jpg',
-    }
+  name: 'Yaakov Chaikin',
+  title: 'Instructor at Coursera',
+  description: "Each of Yaakov's Coursera courses have made it to be a #1 TOP RATED COURSE on Coursera.org. He lectures at Johns Hopkins University and builds web applications for client/customers in person.",
+  featuredImage: 'yaakov.jpg',
+};
 
 const teacher3 = {
-        name: 'Nancy Hughes',
-        title: 'Teaches Web Design in New',
-        description: 'She has to her credit many designs on figma. She has also taught many successful Web Designers',
-        featuredImage: 'nancy.jpg',
-}
+  name: 'Nancy Hughes',
+  title: 'Teaches Web Design in New',
+  description: 'She has to her credit many designs on figma. She has also taught many successful Web Designers',
+  featuredImage: 'nancy.jpg',
+};
 
 const teacher4 = {
-    name: 'Emily Brown',
-    title: 'A Michigan CSS expert',
-    description: 'She takes our student from nothing to something in CSS. By nothing to nothing, she ensures that you get the skill to that of the international standard.',    
-    featuredImage: 'emili.jpg',
-}
+  name: 'Emily Brown',
+  title: 'A Michigan CSS expert',
+  description: 'She takes our student from nothing to something in CSS. By nothing to nothing, she ensures that you get the skill to that of the international standard.',
+  featuredImage: 'emili.jpg',
+};
 
 const teacher5 = {
-    name: 'Stephen Wright',
-    title: 'CLI expert',
-    description: 'He uses the command lines on a daily, he has all you need to become a successful Web developer from the angle of CLI.',    
-    featuredImage: 'steven.jpg',
-}
-
+  name: 'Stephen Wright',
+  title: 'CLI expert',
+  description: 'He uses the command lines on a daily, he has all you need to become a successful Web developer from the angle of CLI.',
+  featuredImage: 'steven.jpg',
+};
 
 const teacher6 = {
-    name: 'Andrew Brown',
-    title: 'Frontend Developer',
-    description: 'He was a Google enplyee as a junior developer, now he teaches our student to Google standard.',
-    featuredImage: 'andrew.jpg',
-}
-
-
+  name: 'Andrew Brown',
+  title: 'Frontend Developer',
+  description: 'He was a Google enplyee as a junior developer, now he teaches our student to Google standard.',
+  featuredImage: 'andrew.jpg',
+};
 
 const teachers = [
-    teacher1,
-    teacher2,
-    teacher3,
-    teacher4,
-    teacher5,
-    teacher6,
-]
-
+  teacher1,
+  teacher2,
+  teacher3,
+  teacher4,
+  teacher5,
+  teacher6,
+];
 
 const generateCards = () => {
-    const placeInHTML = document.querySelector('.featured-teacher')
-    teachers.forEach((cd) => {
-        placeInHTML.insertAdjacentHTML('beforeend',
-        `
+  const placeInHTML = document.querySelector('.featured-teacher');
+  teachers.forEach((cd) => {
+    placeInHTML.insertAdjacentHTML('beforeend',
+      `
         <li class="li-list">
         <div class="imageInd">
         <img class="images-cards" src="./images/${cd.featuredImage}" alt="${cd.name}">
@@ -69,12 +65,10 @@ const generateCards = () => {
           <p>${cd.description}</p>
         </div>
     </li>
-    `
-      )    
-    });
+    `);
+  });
 };
 
-document.addEventListener("DOMContentLoaded", () => {
-    generateCards();
+document.addEventListener('DOMContentLoaded', () => {
+  generateCards();
 });
-
