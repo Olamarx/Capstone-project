55850fbeb4dc38170ce691c6c097ecc7adbcf6d2
OlaoluwaOwoeye <olaoluwaowoeye@gmail.com>
2022-04-15 13:17:12 +0100

    Add two more data

--- js/dist/featured_teachers.dev.js
+++ js/dist/featured_teachers.dev.js
@@ -1,42 +1,54 @@
 "use strict";
 
 var teacher1 = {
+  name: 'Owoeye Olaoluwa',
+  title: 'Student at Microverse',
+  description: 'A student at Microverse as at the time of writing this code and a law student at Obafemi Aolowo University',
+  featuredImage: 'ola.jpeg'
+};
+var teacher2 = {
+  name: 'Obafemi Ireoluwa',
+  title: 'A law student',
+  description: "He is an Intellectual Law enthusiast and the majority leader of the Law Students' Representative Council, Law Students' Society, Obafemi Awolowo University. Anything you could think of about Law.",
+  featuredImage: 'ire.jpeg'
+};
+var teacher3 = {
   name: 'Colleen van Lent Ph.D',
   title: 'Teacher at Coursera',
   description: 'She is a lecturer at School of Information, University of Michigan and she teaches basics of HTML, CSS and Javascript on Coursera',
   featuredImage: 'colleen-van.jpeg'
 };
-var teacher2 = {
+var teacher4 = {
   name: 'Yaakov Chaikin',
   title: 'Instructor at Coursera',
   description: "Each of Yaakov's Coursera courses have made it to be a #1 TOP RATED COURSE on Coursera.org. He lectures at Johns Hopkins University and builds web applications for client/customers in person.",
   featuredImage: 'yaakov.jpg'
 };
-var teacher3 = {
+var teacher5 = {
   name: 'Nancy Hughes',
   title: 'Teaches Web Design in New',
   description: 'She has to her credit many designs on figma. She has also taught many successful Web Designers',
   featuredImage: 'nancy.jpg'
 };
-var teacher4 = {
+var teacher6 = {
   name: 'Emily Brown',
   title: 'A Michigan CSS expert',
   description: 'She takes our student from nothing to something in CSS. By nothing to nothing, she ensures that you get the skill to that of the international standard.',
   featuredImage: 'emili.jpg'
 };
-var teacher5 = {
+var teacher7 = {
   name: 'Stephen Wright',
   title: 'CLI expert',
   description: 'He uses the command lines on a daily, he has all you need to become a successful Web developer from the angle of CLI.',
   featuredImage: 'steven.jpg'
 };
-var teacher6 = {
+var teacher8 = {
   name: 'Andrew Brown',
   title: 'Frontend Developer',
   description: 'He was a Google enplyee as a junior developer, now he teaches our student to Google standard.',
   featuredImage: 'andrew.jpg'
 };
-var teachers = [teacher1, teacher2, teacher3, teacher4, teacher5, teacher6];
+var teachers = [teacher1, teacher2, teacher3, teacher4, teacher5, teacher6, teacher7, teacher8];
 
 var generateCards = function generateCards() {
   var placeInHTML = document.querySelector('.featured-teacher');
@@ -45,6 +57,6 @@ var generateCards = function generateCards() {
   });
 };
 
-document.addEventListener("DOMContentLoaded", function () {
+document.addEventListener('DOMContentLoaded', function () {
   generateCards();
 });
--- js/featured_teachers.js
+++ js/featured_teachers.js
@@ -1,39 +1,56 @@
+
 const teacher1 = {
+  name: 'Owoeye Olaoluwa',
+  title: 'Student at Microverse',
+  description: 'A student at Microverse as at the time of writing this code and a law student at Obafemi Aolowo University',
+  featuredImage: 'ola.jpeg',
+};
+
+const teacher2 = {
+  name: 'Obafemi Ireoluwa',
+  title: 'A law student',
+  description: "He is an Intellectual Law enthusiast and the majority leader of the Law Students' Representative Council, Law Students' Society, Obafemi Awolowo University. Anything you could think of about Law.",
+  featuredImage: 'ire.jpeg',
+};
+
+
+
+const teacher3 = {
   name: 'Colleen van Lent Ph.D',
   title: 'Teacher at Coursera',
   description: 'She is a lecturer at School of Information, University of Michigan and she teaches basics of HTML, CSS and Javascript on Coursera',
   featuredImage: 'colleen-van.jpeg',
 };
 
-const teacher2 = {
+const teacher4 = {
   name: 'Yaakov Chaikin',
   title: 'Instructor at Coursera',
   description: "Each of Yaakov's Coursera courses have made it to be a #1 TOP RATED COURSE on Coursera.org. He lectures at Johns Hopkins University and builds web applications for client/customers in person.",
   featuredImage: 'yaakov.jpg',
 };
 
-const teacher3 = {
+const teacher5 = {
   name: 'Nancy Hughes',
   title: 'Teaches Web Design in New',
   description: 'She has to her credit many designs on figma. She has also taught many successful Web Designers',
   featuredImage: 'nancy.jpg',
 };
 
-const teacher4 = {
+const teacher6 = {
   name: 'Emily Brown',
   title: 'A Michigan CSS expert',
   description: 'She takes our student from nothing to something in CSS. By nothing to nothing, she ensures that you get the skill to that of the international standard.',
   featuredImage: 'emili.jpg',
 };
 
-const teacher5 = {
+const teacher7 = {
   name: 'Stephen Wright',
   title: 'CLI expert',
   description: 'He uses the command lines on a daily, he has all you need to become a successful Web developer from the angle of CLI.',
   featuredImage: 'steven.jpg',
 };
 
-const teacher6 = {
+const teacher8 = {
   name: 'Andrew Brown',
   title: 'Frontend Developer',
   description: 'He was a Google enplyee as a junior developer, now he teaches our student to Google standard.',
@@ -47,6 +64,8 @@ const teachers = [
   teacher4,
   teacher5,
   teacher6,
+  teacher7,
+  teacher8,
 ];
 
 const generateCards = () => {
